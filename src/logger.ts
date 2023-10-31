904ab1a3b7701ab3e09350e808a54e1fcdfc8f42
William Hurley <wjhurley@comcast.net>
2023-10-30 21:36:37 -0700

    Improve log readability by aligning, colorizing, and formatting output

--- src/logger.ts
+++ src/logger.ts
@@ -1,28 +1,14 @@
 import * as Winston from 'winston';
 
 export const logger = Winston.createLogger({
-    defaultMeta: { service: 'netsuite-schema-scraper' },
-    // format: Winston.format.json(),
-    levels: Winston.config.syslog.levels,
+    format: Winston.format.combine(
+        Winston.format.colorize(),
+        Winston.format.timestamp(),
+        Winston.format.align(),
+        Winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`),
+    ),
+    levels: Winston.config.cli.levels,
     transports: [
-        //
-        // - Write all logs with importance level of `error` or less to `error.log`
-        // - Write all logs with importance level of `info` or less to `combined.log`
-        //
-        new Winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new Winston.transports.File({ filename: 'combined.log' }),
+        new Winston.transports.Console(),
     ],
 });
-
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new Winston.transports.Console({
-        format: Winston.format.combine(
-            Winston.format.colorize(),
-            // Winston.format.json(),
-        ),
-    }));
-}
